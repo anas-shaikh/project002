69753ba719bf84f96c92a4aa07fa279368544d28
anas-shaikh <56759090+anas-shaikh@users.noreply.github.com>
2020-06-22 06:46:36 -0700

    updated transaction balance code
    code in transaction balance component and linked file in app.js and created custom class for transaction balance component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import './App.css';
 //Importing Files
 //importing Header file
 import { Header } from './components/header'
+//importing Transaction balance
+import { Balance } from './components/transaction_balance'
+
 
 function App() {
   return (
     <div>
       <Header/>
+      <div className="balance">
+        <Balance/>
+      </div>
     </div>
   );
 }
